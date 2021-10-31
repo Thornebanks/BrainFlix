82cbff1f9a637d45aeb796368cc4f458a6e8523e
Thornebanks <akshays.singh613@gmail.com>
2021-10-31 19:25:51 -0400

    tablet header and video done

--- akshay-singh-brainflix/src/components/Header/Header.jsx
+++ akshay-singh-brainflix/src/components/Header/Header.jsx
@@ -1,20 +1,35 @@
-import logo from '../../assests/Logo/BrainFlix-logo.svg';
-import hero from '../../assests/Images/Mohan-muruge.jpg'
-import './header.scss';
+import logo from "../../assests/Logo/BrainFlix-logo.svg";
+import hero from "../../assests/Images/Mohan-muruge.jpg";
+import "./header.scss";
 
-function Header(){
-    return (
-        <header className="header">
-        <img className="header__img" src={logo} alt="BrainFlix logo" />
-        <div className="header__search">
-          <form className="header__form">
-            <input className="header__form header__form--search" type="text" name="" id="" placeholder="Search" />
-            <div className="header__btn"><button className="header__btn header__btn--style" onClick={(event) => event.preventDefault()}>upload</button></div>
-          </form>
-          <div className="header__hero"><img src={hero} alt="hero image" /></div>
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__img" src={logo} alt="BrainFlix logo" />
+      <div className="header__search">
+        <form className="header__form">
+          <input
+            className="header__form header__form--search"
+            type="text"
+            name=""
+            id=""
+            placeholder="Search"
+          />
+          <div className="header__btn">
+            <button
+              className="header__btn header__btn--style"
+              onClick={(event) => event.preventDefault()}
+            >
+              upload
+            </button>
+          </div>
+        </form>
+        <div className="header__hero">
+          <img src={hero} alt="hero image" />
         </div>
-      </header>
-    );
+      </div>
+    </header>
+  );
 }
 
 export default Header;
